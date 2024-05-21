dd06826fa8b8226035b8762f616b27ff36d08baa
Sanjeev banmala <banmalasanjeev@gmail.com>
2024-05-21 17:32:56 +0545

    addeddb migration code

---
+++ src/db/sql/db_migrate.py
@@ -0,0 +1,58 @@
+import os
+import argparse
+
+from utils.database import databaseConnect, databaseDisconnect
+
+
+def migration_down():
+    schemas = ["raw", "dbo"]
+    conn = databaseConnect()
+    cur = conn.cursor()
+    for schema in schemas:
+        cur.execute(
+            f""" 
+        DROP SCHEMA IF EXISTS {schema} CASCADE;
+        """
+        )
+    print("[+] VyagutaInfo Database cleaned!\n")
+    conn.commit()
+    databaseDisconnect(conn, cur)
+
+
+def migration_up():
+    conn = databaseConnect()
+    cur = conn.cursor()
+    directories = ["./sql/migrations", "./sql/procedures"]
+    for directory in directories:
+        for filename in sorted(os.listdir(directory)):
+            if filename.endswith(".sql"):
+                with open(os.path.join(directory, filename), "r") as f:
+                    sql_command = f.read()
+                    try:
+                        cur.execute(sql_command)
+                        conn.commit()
+                        print(f"[+] Executed {filename}\n")
+                    except Exception as e:
+                        conn.rollback()
+                        print(f"[-] Failed to execute {filename}: ", e)
+    databaseDisconnect(conn, cur)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--up",
+        action="store_true",
+        help="Run migration_up for Vyaguta Leave Info tables.",
+    )
+    parser.add_argument(
+        "--down",
+        action="store_true",
+        help="Run migration_down for Vyaguta Leave Info tables.",
+    )
+    args = parser.parse_args()
+
+    if args.up:
+        migration_up()
+    elif args.down:
+        migration_down()
